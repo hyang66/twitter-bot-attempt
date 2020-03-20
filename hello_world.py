b0daeb353a4c17337fb0012589fa5c7ebec16b98
hyang66 <mistyfeather1@gmail.com>
2020-03-20 19:36:50 -0400

    edted for deployment

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,14 @@
 import tweepy
-from secrets import *
+from os import environ
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
 # Create API object
 api = tweepy.API(auth)
--- read_from_file.py
+++ read_from_file.py
@@ -1,11 +1,16 @@
 import tweepy
 from time import sleep
-from secrets import *
 hours = 60 * 60
+
+from os import environ
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
 # Create API object
 api = tweepy.API(auth)
