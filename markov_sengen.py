80194793c51818ff1b934168b61f84901ca02f52
hyang66 <mistyfeather1@gmail.com>
2020-03-26 13:28:07 -0400

    editd so it tweets

--- markov_sengen.py
+++ markov_sengen.py
@@ -88,5 +88,23 @@ def generate_sentence(wl, sw):
         gen_word = (*(gen_word[1:]), next_word_list[rand])
     return sentence
 
-for i in range(30):
-    print(generate_sentence(word_links, starting_words))
+
+import tweepy
+from os import environ
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+# Create API object
+api = tweepy.API(auth)
+
+# Create a tweet
+api.update_status(generate_sentence(word_links, starting_words))
+
+
