22f5cf648626a5eb22eea0cfe0f6e20b0422710e
Hanna Yang <34112076+hyang66@users.noreply.github.com>
2020-11-13 23:44:46 -0500

    update documentation; small aesthetic fixes

--- markov_sengen.py
+++ markov_sengen.py
@@ -1,34 +1,18 @@
 import random
 from secrets import *
-# class ProbTree:
 
-    # def __init__(self):
-        # self.l = []
-        # self.total = 0 
-
-    # def add(self, item):
-        # self.total += 1
-        # self.l.append(item)
-        # # self.total += 1
-        # # if item in self.d:
-            # # self.d[item] += 1
-        # # else:
-            # # self.d[item] = 1
-
-    # def __str__(self):
-        # return "|| dict: " + str(self.l) + " total items: " + str(self.total) + " ||"
-
-# process file into probability sequences
 PUNCTUATION = {"\"", "-", ",", ".", "(", ")", "!", "?", " "}
+NO_LEFT_SPACE = {"\"", "-", ",", ".", ")", "!", "?", " "}
+NO_RIGHT_SPACE = {"\"", "-", "(", " "}
 
 f = open("./corpus.txt", "r")
+# we define sentences as a some text between new lines
 lines = f.read().split("\n")
 
 def process(s):
     """
     given a string, process it into a list such that:
-    it is split on spaces,
-    and punctuations are on thier own
+    it is split on spaces, and punctuation
     """
     ans = []
     current_sampling = ""
@@ -37,7 +21,7 @@ def process(s):
             if current_sampling != "":
                 ans.append(current_sampling) 
                 current_sampling = ""
-            #also add in non-space punctuation
+            # also add in non-space punctuation
             if s[i] != " ":
                 ans.append(s[i])
         else:
@@ -47,6 +31,11 @@ def process(s):
     return ans
 
 def generate_links(lines, n):
+    """
+    generate dictionary of sequences of words that are linked
+    using a frame of size n
+    return list of words starting sentences and word frequencies
+    """
     starting_words = []
     word_links = dict()
     for line in lines:
@@ -57,6 +46,9 @@ def generate_links(lines, n):
         for ind, word in enumerate(line):
             endpoint = min(ind + n, len(line))
             word_grouping = tuple(line[j] for j in range(ind, endpoint))
+            # value of a word link is a list of words appearing immediately after
+            # if the same word appears multiple times, it appears multiple times in
+            # the list
             if word_grouping in word_links:
                 if ind + n < len(line):
                     word_links[word_grouping].append(line[ind + n])
@@ -71,23 +63,34 @@ def generate_links(lines, n):
     return starting_words, word_links
 
 starting_words, word_links = generate_links(lines, 1)
-# for word in word_links:
-    # print(str(word) + " : " + str(word_links[word]))
 
 def generate_sentence(wl, sw):
+    """
+    Given a list of word links (wl) and starting words (sw),
+    pick a random starting word link and generate sentences
+    """
     rand = random.randrange(len(sw))
     gen_word = sw[rand]
     sentence = ""
     for word in gen_word[:-1]:
         sentence += word + " "
+    # stop generating at the end of a line
+    # corresponds to paragraph
     while not gen_word[-1] == "\n":
-        sentence += gen_word[-1] + " "
+        new_word = gen_word[-1]
+        # omit left space
+        if new_word in NO_LEFT_SPACE:
+            sentence = sentence[:-1]
+        sentence += new_word + " "
+        # omit right space
+        if new_word in NO_RIGHT_SPACE:
+            sentence = sentence[:-1]
         next_word_list = wl[gen_word]
-        # print(gen_word)
-        # print(next_word_list)
         rand = random.randrange(len(next_word_list))
+        # shift generated word over by one, and continue
         gen_word = (*(gen_word[1:]), next_word_list[rand])
-    return sentence
+    # strip out final space
+    return sentence[:-1]
 
 
 import tweepy
@@ -101,7 +104,7 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 # Create API object
 api = tweepy.API(auth)
 
-# Create a tweet
+# send a tweet!
 while True:
     api.update_status(generate_sentence(word_links, starting_words))
     sleep(60*60)
